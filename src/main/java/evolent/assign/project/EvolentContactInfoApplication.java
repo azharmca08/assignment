4c2ae611ab9a58372fba4cd883b3ae4370084f39
Md Azhar <azhar.mca08@gmail.com>
2021-05-22 22:24:23 +0530

    initial commit

---
+++ src/main/java/evolent/assign/project/EvolentContactInfoApplication.java
@@ -0,0 +1,13 @@
+package evolent.assign.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolentContactInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolentContactInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/evolent/assign/project/EvolentContactInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package evolent.assign.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolentContactInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
